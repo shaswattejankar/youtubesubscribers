a967c6aabb949b25917233e280381b3ab5713ca9
shaswattejankar <shaswattejankar+github@gmail.com>
2023-09-12 21:37:37 +0530

    .

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 // Connect to DATABASE
 
 const DATABASE_URL = process.env.DATABASE_URL
+
 // mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 // const db = mongoose.connection
 // db.on('error', (err) => console.log(err))
